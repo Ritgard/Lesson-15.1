39fb613ae69b4009d7dbaeecc6e029e21d36e9ac
monahov <m0untlab@yandex.ru>
2025-10-01 15:15:50 +0300

    initial commit

---
+++ Lesson 15.1.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector> // optional
+#include <string>
+using namespace std;
+
+class Adress
+{
+private:
+	string nameCity{};
+	string nameStreet{};
+	int numberHouse{};
+	int numberPalace{};
+public:
+	Adress(const string& nameCity, const string& nameStreet, int numberHouse, int numberPalace) :
+		nameCity(nameCity), nameStreet(nameStreet), numberHouse(numberHouse), numberPalace(numberPalace) { }
+	string getOutputAdress() const
+	{
+		return nameCity + ", " + nameStreet + ", " + to_string(numberHouse) + ", " + to_string(numberPalace);
+	}
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	ifstream inputFile("in.txt");
+	if (!inputFile.is_open()) cerr << "Error opening in.txt";
+
+	int n;
+	inputFile >> n;
+	inputFile.ignore(10000000, '\n');
+
+	Adress** adresses = new Adress*[n];
+	for (int i{}; i < n; ++i)
+	{
+		string city, street, houseStr, palaceStr;
+
+		getline(inputFile, city);
+		getline(inputFile, street);
+		getline(inputFile, houseStr);
+		getline(inputFile, palaceStr);
+
+		int houseNumber = stoi(houseStr);
+		int palaceNumber = stoi(palaceStr);
+
+		houseNumber = stoi(houseStr);
+		palaceNumber = stoi(palaceStr);
+
+		adresses[i] = new Adress(city, street, houseNumber, palaceNumber);
+	}
+
+	inputFile.close();
+
+	ofstream outputFile("out.txt");
+	if (!outputFile.is_open())
+	{
+		/*for (int i{}; i < n; i++)
+		{
+			delete adresses[i];
+		}
+		delete[] adresses;*/
+		cerr << "Error opening out.txt";
+	}
+	outputFile << n << endl;
+	for (int i{n - 1}; i >= 0; --i)
+	{
+		outputFile << adresses[i]->getOutputAdress() << endl;
+	}
+	outputFile.close();
+	for (int i{}; i < n; i++)
+	{
+		delete adresses[i];
+	}
+	delete[] adresses;
+	return EXIT_SUCCESS;
+}
